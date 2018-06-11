43bcd167dbf490e378e3fcd8a004d2ec26d5781a
Paula Moraes <30325208+paulaksm@users.noreply.github.com>
2018-06-11 14:27:43 -0300

    Update configure_all.sh

--- configure_all.sh
+++ configure_all.sh
@@ -43,7 +43,7 @@ sudo apt-get install spotify-client
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get install sublime-text
+sudo apt update && sudo apt-get install sublime-text
 
 # CONFIG SUBLIME
 dd of=~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings << EOF
